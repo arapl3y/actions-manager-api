a0de156730ea3db36d0e914371bbd90a7baed997
arapl3y <rapley3@gmail.com>
2019-08-22 16:52:30 +1000

    Add validation to routes

--- start/routes.js
+++ start/routes.js
@@ -25,7 +25,7 @@ Route.post('login', 'AuthController.login')
 
 // Unprotected users
 Route.get('users', 'UserController.index')
-Route.post('users/new', 'UserController.store')
+Route.post('users', 'UserController.store').validator('StoreUser')
 Route.post('users/forgotPassword', 'ForgotPasswordController.store')
 Route.put('users/forgotPassword/:token/:email', 'ForgotPasswordController.update')
 
@@ -38,11 +38,11 @@ Route.group(() => {
   Route.put('users/:id', 'UserController.update')
 
   // Actions
-  Route.get('actions/list', 'ActionController.index')
+  Route.get('actions', 'ActionController.index')
   Route.get('actions/:id', 'ActionController.show')
   Route.put('actions/:id', 'ActionController.update').validator('UpdateAction')
-  Route.post('actions/new', 'ActionController.store').validator('Action')
-  Route.delete('actions/:id/delete', 'ActionController.destroy')
+  Route.post('actions', 'ActionController.store').validator('CreateAction')
+  Route.delete('actions/:id', 'ActionController.destroy')
 }).middleware(['auth'])
 
 
